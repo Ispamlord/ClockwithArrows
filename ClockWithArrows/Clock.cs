57068dde22cc708666d1f34c0fd5a9cd320e2175
Ispamlord <146575918+Ispamlord@users.noreply.github.com>
2024-06-05 18:28:41 +0700

    Добавьте файлы проекта.

---
+++ ClockWithArrows/Clock.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClockWithArrows
+{
+    public class digital_clock : IDigital
+    {
+        public void gettime(decimal Hour, decimal min)
+        {
+            DateTime date = DateTime.Now;
+            string result = "";
+            Hour -= date.Hour;
+            min -= date.Minute;
+            
+            if (min < 0)
+            {
+                Hour -= 1;
+                min += 60;
+            }
+            if (Hour < 9)
+            {
+                result += "0" + Hour;
+            }
+            else
+            {
+                result += Hour;
+            }
+            result += ":";
+            if (min < 9)
+            {
+                result += "0" + min;
+            }
+            else
+            {
+                result += min;
+            }
+           Console.WriteLine("расхождение во времени"+result);
+        }
+    }
+    public interface IDigital {
+        void gettime(decimal Hour, decimal min);
+    }
+
+    
+    public class Adapter : IDigital
+    {
+        private IAnalog analog;
+        public Adapter(IAnalog analog)
+        {
+            this.analog = analog;
+        }
+        public void gettime(decimal Hour, decimal min)
+        {
+            analog.getangle(Hour, min);
+        }
+    }
+    public interface IAnalog {
+        void getangle(decimal Hour, decimal min);
+    }
+    public class CLockWithArrow : IAnalog 
+    {
+        public void getangle(decimal Hour, decimal min)
+        {
+            string result = "";
+            DateTime date = DateTime.Now;
+            Hour -= date.Hour;
+            min -= date.Minute;
+            min *= 6;
+            min += Hour*360;            
+            result += min;
+
+            Console.WriteLine("Поверните стрелку на " + result + " градусов");
+        }
+    }
+}
+//if (Hour < 0)
+//{
+//    Hour
+//    Hour = -Hour * 30 + (-min / 60 * 30) + 360;
+//}
+//else
+//{
+//    Hour = -Hour * 30 + (-min / 60 * 30);
+//}
+//min = -min * 360 / 60;
+//if (min < 0)
+//{
+//    min += 360;
+//}
+//if (Hour < 0)
+//{
+//    Hour += 360;
+//}
---
+++ ClockWithArrows/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClockWithArrows
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите время цифровых часов");
+            string timer = Console.ReadLine();
+            string[] time = timer.Split(':') ;
+            int hour = int.Parse(time[0]);
+            if (hour > 23 || hour<0)
+                throw new AccessViolationException("час не может быть отрицательным или больше 23");
+            int min = int.Parse(time[1]);
+            if(min>59 || min<0)
+                throw new AccessViolationException("Минута не может быть отрицательным или больше 59");
+            DateTime dt = DateTime.Now;
+
+            Console.WriteLine("Время на часах: "+dt.Hour+":"+dt.Minute);
+            //IDigital ae = new digital_clock();
+            //ae.gettime(hour, min);
+            IDigital digital = new Adapter(new CLockWithArrow());
+            digital.gettime(hour,min) ;
+        }
+    }
+}
